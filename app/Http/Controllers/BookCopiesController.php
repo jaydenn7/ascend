eb27eea3834901c9f0292f42954b36348b22fca1
Jaden Shepherd <ijaden.shepherd@gmail.com>
2024-08-12 14:17:43 +0100

    3-1 Add tables for Requested and Borrowed Copies

--- app/Http/Controllers/BookCopiesController.php
+++ app/Http/Controllers/BookCopiesController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\BookCopyResource;
 use App\Models\BookCopy;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BookCopiesController extends Controller
 {
@@ -17,7 +19,19 @@ class BookCopiesController extends Controller
                     ->whereAvailable()
                     ->applySearchFiltersFrom($request)
                     ->get()
-            )
+            ),
+            "requested_copies" => fn () => BookCopyResource::collection(
+                BookCopy::query()
+                    ->with("book.authors")
+                    ->whereHas("borrowRequests", fn (Builder $query) => $query->whereBelongsTo(Auth::user()))
+                    ->get()
+            ),
+            "borrowed_copies" => fn () => BookCopyResource::collection(
+                BookCopy::query()
+                    ->with("book.authors")
+                    ->whereHas("borrows", fn (Builder $query) => $query->whereBelongsTo(Auth::user()))
+                    ->get()
+            ),
         ]);
     }
 }
--- resources/js/Pages/BookCopies/Index.vue
+++ resources/js/Pages/BookCopies/Index.vue
@@ -1,27 +1,43 @@
 <template>
-    <AuthenticatedLayout>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                <h1 class="text-xl font-bold">Book copies</h1>
-                <div class="bg-white">
-                    <ResponsiveTable :data="book_copies.data" :headers="headers">
-                        <template #actions="actionProps">
-                            <DropdownLink
-                                method="post"
-                                :href="route('borrow-requests.store', actionProps.row.id)"
-                                class="flex items-center space-x-2"
-                                :data="{
+  <AuthenticatedLayout>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <h1 class="text-xl font-bold">Borrowed copies</h1>
+        <div class="bg-white">
+          <ResponsiveTable :data="borrowed_copies.data" :headers="headers"></ResponsiveTable>
+        </div>
+      </div>
+    </div>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <h1 class="text-xl font-bold">Requested copies</h1>
+        <div class="bg-white">
+          <ResponsiveTable :data="requested_copies.data" :headers="headers"></ResponsiveTable>
+        </div>
+      </div>
+      <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+          <h1 class="text-xl font-bold">Book copies</h1>
+          <div class="bg-white">
+            <ResponsiveTable :data="book_copies.data" :headers="headers">
+              <template #actions="actionProps">
+                <DropdownLink
+                    method="post"
+                    :href="route('borrow-requests.store', actionProps.row.id)"
+                    class="flex items-center space-x-2"
+                    :data="{
                                     book_copy_id: actionProps.row.id
                                 }"
-                            >
-                                <span>Request</span>
-                            </DropdownLink>
-                        </template>
-                    </ResponsiveTable>
-                </div>
-            </div>
+                >
+                  <span>Request</span>
+                </DropdownLink>
+              </template>
+            </ResponsiveTable>
+          </div>
         </div>
-    </AuthenticatedLayout>
+      </div>
+    </div>
+  </AuthenticatedLayout>
 </template>
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
@@ -33,15 +49,15 @@ import DropdownLink from "@/Components/DropdownLink.vue";
 import {collect} from "collect.js";
 
 export default {
-    props: ['book_copies'],
-    computed: {
-        headers() {
-            return {
-                id: 'ID',
-                name: {label: 'Name', displayAs: (bookCopy) => bookCopy.book.name},
-                authors: {label: 'Authors', displayAs: (bookCopy) => collect(bookCopy.book.authors).pluck('name').join(', ')},
-            };
-        }
+  props: ['book_copies', "requested_copies", "borrowed_copies"],
+  computed: {
+    headers() {
+      return {
+        id: 'ID',
+        name: {label: 'Name', displayAs: (bookCopy) => bookCopy.book.name},
+        authors: {label: 'Authors', displayAs: (bookCopy) => collect(bookCopy.book.authors).pluck('name').join(', ')},
+      };
     }
+  }
 }
 </script>
